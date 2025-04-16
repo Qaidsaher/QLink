ae148e6d51b00ad64b486b6b46fec9e00719dc66
SaherQaid <saherqaid2020@gmail.com>
2025-04-16 17:34:46 +0300

    updating : user (model & migration & factory)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,10 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'username'          => $this->faker->unique()->userName,
+            'role'              => 'user',
+            'bio'               => $this->faker->sentence,
+            'avatar'            => $this->faker->imageUrl,
             'remember_token' => Str::random(10),
         ];
     }

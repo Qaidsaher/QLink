67d8437fb3424b5dd1ab2837da4ffb08ac8a736d
SaherQaid <saherqaid2020@gmail.com>
2025-04-16 17:41:53 +0300

    creating : comment (model & migration & factory)

---
+++ app/Models/Comment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    /** @use HasFactory<\Database\Factories\CommentFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'post_id', 'user_id', 'content', 'parent_comment_id'
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Comment::class, 'parent_comment_id');
+    }
+}
